ced16d1b82e83d121f4299ea01f384a6556a15ca
Alex <alexandra.samarina.v@yandex.ru>
2021-02-12 14:49:23 +0300

    1 commit

---
+++ CLib1/CLib1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CLib1
+{
+    public class Class1
+    {
+        public static void output_mas(ref DataGridView grid, params int[] a)
+        {
+            grid.RowCount = 2;
+            grid.ColumnCount = a.GetLength(0);
+
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                grid.Rows[0].Cells[i].Value = i + 1;
+                grid.Rows[1].Cells[i].Value = a[i];
+            }
+            int Width = 0;
+            for (int j = 0; j < grid.ColumnCount; j++)
+            {
+                Width += grid.Columns[j].Width;
+                if (Width > 1500)
+                    grid.Width = 1500;
+                else
+                    grid.Width = Width;
+            }
+
+        }
+        public static void enter_mas(int n, params int[] massivchikk)
+        {
+            Random a = new Random();
+            for (int i = 0; i < n; i++)
+                massivchikk[i] = (int)(a.Next(10));/// потом изменить на 1000
+        }
+        public static void set_mass(int x, ref int[] rezmas, params int[] massivchikk)
+        {
+           rezmas[0] = 0;
+
+            for (int i = 0; i < massivchikk.GetLength(0); i++)
+            {
+                if ((rezmas[0] < 1) && (massivchikk[i] == x))
+                    rezmas[0] = i + 1;
+            }
+            if (rezmas[0] == 0)
+                MessageBox.Show("Элемент,который равен х, не нашелся");         
+             }    
+        }
+
+
+    }
+
---
+++ WiFsA/WiFsA/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WiFsA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(331, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 488);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
